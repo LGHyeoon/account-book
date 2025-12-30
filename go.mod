4f64781efd1d268607129490de7600b5f9107590
LGHyeoon <a01043355935@gmail.com>
2025-12-30 21:42:13 +0900

    Go + HTMX 프로젝트 초기화

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LGHyeoon/account-book
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	handler1 := func(w http.ResponseWriter, r *http.Request) {
+		tmp1 := template.Must(template.ParseFiles("html/index.html"))
+		tmp1.Execute(w, nil)
+	}
+
+	http.HandleFunc("/", handler1)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
