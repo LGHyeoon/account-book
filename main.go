c39385349f41dc829850b05548f3b17ea338c45f
LGHyeoon <a01043355935@gmail.com>
2026-01-03 11:01:54 +0900

    1. 로그인 시 JSON으로 받은 아이디, 비밀번호 값을 받아오도록 로직 수정 2. 회원가입 URL 매핑 추가

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"text/template"
 )
 
 func main() {
-	handler1 := func(w http.ResponseWriter, r *http.Request) {
+	// Handler 선언
+	loginViewHandler := func(w http.ResponseWriter, r *http.Request) {
 		tmp1 := template.Must(template.ParseFiles("resources/views/login.html"))
 		tmp1.Execute(w, nil)
 	}
 
-	handler2 := func(w http.ResponseWriter, r *http.Request) {
-		log.Print(r.PostFormValue("userId") + " / " + r.PostFormValue("userPwd"))
+	loginHandler := func(w http.ResponseWriter, r *http.Request) {
+		// 260103 내용
+		// 프론트에서 JSON으로 값을 넘길 경우에는 아래와 같이 unmarshal 처리를 해서 데이터를 가져와야한다. (기존의 ioutil.ReadAll은 deprecated 됨)
+		// 그 외의 경우로 프론트에서 FormData API를 통해 넘길 경우에는 r.PostFormValue("데이터명") 으로 가져온다.
+		body, _ := io.ReadAll(r.Body)
+		keyVal := make(map[string]string)
+		json.Unmarshal(body, &keyVal)
+
+		userId := keyVal["userId"]
+		userPwd := keyVal["userPwd"]
+
+		log.Print(userId + " / " + userPwd)
 	}
 
-	http.HandleFunc("/", handler1)
-	http.HandleFunc("/login", handler2)
+	regViewHandler := func(w http.ResponseWriter, r *http.Request) {
+		tmp := template.Must(template.ParseFiles("resources/views/register.html"))
+		tmp.Execute(w, nil)
+	}
+
+	// 매핑
+	http.HandleFunc("/", loginViewHandler)
+	http.HandleFunc("/doLogin", loginHandler)
+	http.HandleFunc("/register", regViewHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
